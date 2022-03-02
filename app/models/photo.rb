7a31472d111f13bc80447cfa3964ef2032612a61
mellowmorgan <morgan.waites@gmail.com>
2022-03-02 11:47:16 -0800

    add validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,12 @@
 class Photo < ApplicationRecord
   validates :file_path, presence: true
+  validate :no_duplicates
+  validates :file_path, format: { with: URI.regexp }
 end
+  def no_duplicates
+    results = Photo.where(file_path: self.file_path)
+    if (results.any?)
+      errors.add(:file_path, "Photo url already exists.")
+    end
+
+  end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,10 @@
 class Quote < ApplicationRecord
   validates :content, presence: true
+  validate :no_duplicates
+  def no_duplicates
+    results = Quote.where(content: self.content)
+    if (results.any?)
+      errors.add(:content, "Quote already exists.")
+    end
+  end  
 end
