d6e478297e38632ece4dc890fe101533279ccb1f
mellowmorgan <morgan.waites@gmail.com>
2022-03-03 17:31:30 -0800

    try again creating db

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_200133) do
+ActiveRecord::Schema.define(version: 2022_03_04_012813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "photos", force: :cascade do |t|
     t.string "file_path"
-    t.datetime "created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "quotes", force: :cascade do |t|
     t.string "content"
-    t.datetime "created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
