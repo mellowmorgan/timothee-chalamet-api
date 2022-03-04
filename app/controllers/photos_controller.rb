867dcaa10ac6c94e8edc1c5e02d21198daf43a8e
mellowmorgan <morgan.waites@gmail.com>
2022-03-03 17:43:37 -0800

    update get all photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,7 +5,7 @@ class PhotosController < ApplicationController
   end
   
   def index
-    @photos = Photo.order(created_at: :desc)
+    @photos = Photo.all.order(created_at: :desc)
     json_response(@photos)
   end
 
