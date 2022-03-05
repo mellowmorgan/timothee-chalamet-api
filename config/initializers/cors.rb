071b09b53e26494d8e1b43313c3f5bcc6d19747a
mellowmorgan <morgan.waites@gmail.com>
2022-03-04 18:48:56 -0800

    change cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://mellowmorgan.github.io/react-chalamet-frontend/'
 
     resource '*',
       headers: :any,
