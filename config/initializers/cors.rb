a13e5d9dd5885b46a5806e64ea97ebd6ee060fda
mellowmorgan <morgan.waites@gmail.com>
2022-03-05 11:57:45 -0800

    go back to config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,6 +14,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   end
-  
-
-Rails.application.config.hosts << "timothee-chalamet-api.herokuapp.com", "https://mellowmorgan.github.io/react-chalamet-frontend/"
